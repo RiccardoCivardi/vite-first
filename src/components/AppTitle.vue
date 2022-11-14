8833451c63c0e765d8889cc9ab2b8cdf3c1297b5
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-11-14 19:00:45 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'AppTitle'
+}
+</script>
+
+<template>
+
+  <div class="container">
+
+    <h1>La mia prima app con Vite!</h1>
+
+  </div>
+
+  
+</template>
+
+
+<style scoped>
+
+h1{
+  font-size: 4rem;
+  color: white;
+  text-align: center;
+}
+
+</style>
